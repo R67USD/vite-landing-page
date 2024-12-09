ec590e604f2716b538f9962cf1852741fc2d24f6
nishumurmu <nnmurmu@enacton.com>
2024-12-09 16:56:05 +0530

    fix(mobile): servicetwo component ui fixed.

--- src/components/ServicesTwo.tsx
+++ src/components/ServicesTwo.tsx
@@ -1,16 +1,10 @@
 const ServiceCard = ({ title, description, image, name }: any) => (
   <div className="bg-purple-card rounded-[33.81px] p-8 relative">
-    <div className="w-[96.6px] h-[96.6px] bg-orange-500 rounded-full absolute -top-12 right-8">
+    <div className="max-sm:w-[62.2px] w-[96.6px] max-sm:h-[62.2px] h-[96.6px] bg-orange-500 rounded-full absolute max-sm:-top-[2rem] -top-12 right-8">
       <img
         src={image}
         alt={name}
-        className="w-full h-full"
-        style={{
-          width: "60px",
-          height: "60px",
-          marginTop: "17px",
-          marginLeft: "17px",
-        }}
+        className="max-sm:w-[40px] w-[60px] max-sm:h-[40px] h-[60px] mt-[17px] ml-[17px] max-sm:mt-[12px] max-sm:ml-[12px]"
       />
     </div>
     <h3 className="text-white card-title mt-[63px] mb-4">{title}</h3>
@@ -21,16 +15,16 @@ const ServicesTwo = () => {
   return (
     <section className="bg-purple-darker py-16">
       <div className="container mx-auto pt-[153px] pb-[212px]">
-        <h2 className="services-title">
+        <h2 className="text-[86.66px] font-bold leading-[104.41px] petrov-sans max-sm:text-[55px] text-center">
           <span className="text-white">Our </span>
           <span className="text-orange-500">Services</span>
         </h2>
-        <p className="services-desc-two">
+        <p className="text-center text-white max-w-3xl mx-auto mb-8 petrov-sans md:mb-16 px-4">
           Boost Your Brand's Presence With Targeted Social Media Strategies
           Designed To Engage, Grow, And Convert Audiences Across All Platforms.
           Let Us Help You Connect Authentically And Drive Impactful Results.
         </p>
-        <div className="max-w-[1100px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-[46.18px]">
+        <div className="max-w-[1100px] max-sm:max-w-[320px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[46.18px]">
           <ServiceCard
             title="Political Campaigns"
             description="In The Fast-Paced World Of Politics, Every Connection Counts. We Help You Create Campaigns That Resonate With Constituents And Drive Voter Engagement."
